b0fca9af59756637c1b85c0a78bef07a785711e2
mahmudul <mahmudul35-218@diu.edu.bd>
2025-06-01 10:51:15 +0600

    add pagination

--- src/pages/address/district/index.jsx
+++ src/pages/address/district/index.jsx
@@ -22,24 +22,29 @@ const District = () => {
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const [lastPage, setLastPage] = useState(1);
-  const [nextPageUrl, setNextPageUrl] = useState(null);
-  const [prevPageUrl, setPrevPageUrl] = useState(null);
+  // const [lastPage, setLastPage] = useState(1);
+  // const [nextPageUrl, setNextPageUrl] = useState(null);
+  // const [prevPageUrl, setPrevPageUrl] = useState(null);
+  const [perPage, setPerPage] = useState(10);
+  const [totalRows, setTotalRows] = useState(0);
 
-  
+  console.log("totalRows",totalRows)
+  console.log("currentPage",currentPage)
+
   // fetch banner data
   const fetchData = useCallback(
     async (banner) => {
       try {
         setLoading(true);
-        const response = await NetworkServices.District.index(currentPage);
+        const response = await NetworkServices.District.index(currentPage,perPage);
         if (response?.status === 200 || response?.status === 201) {
           setData(response?.data?.data?.data);
-          setCurrentPage(response?.data?.data?.current_page);
-          setLastPage(response?.data?.data?.last_page);
-          setNextPageUrl(response?.data?.data?.next_page_url);
-          setPrevPageUrl(response?.data?.data?.prev_page_url);
+          // setCurrentPage(response?.data?.data?.current_page);
+          // setLastPage(response?.data?.data?.last_page);
+          // setNextPageUrl(response?.data?.data?.next_page_url);
+          // setPrevPageUrl(response?.data?.data?.prev_page_url);
           setLoading(false);
+          setTotalRows(response?.data?.data?.total || 0);
         }
       } catch (error) {
         if (error) {
@@ -48,12 +53,21 @@ const District = () => {
         }
       }
     },
-    [currentPage]
+    [currentPage,currentPage,perPage]
   );
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [fetchData]);
 
+  const handlePageChange = (page) => {
+    if (!loading) {
+      setCurrentPage(page);
+    }
+  };
+  const handleRowsPerPageChange = (newPerPage, page) => {
+    setPerPage(newPerPage);
+    setCurrentPage(page);
+  };
   // delete data
   const destroy = async (id) => {
     try {
@@ -85,7 +99,12 @@ const District = () => {
           </Link>
 
           <span>
-            <span className="bg-primary text-white btn btn-sm" onClick={() => destroy(row?.id)}>Delete</span>
+            <span
+              className="bg-primary text-white btn btn-sm"
+              onClick={() => destroy(row?.id)}
+            >
+              Delete
+            </span>
           </span>
         </div>
       ),
@@ -108,14 +127,24 @@ const District = () => {
         <SkeletonTable />
       ) : (
         <>
-          <DataTable columns={columns} data={data} />
-          <Pagination
+          <DataTable
+            columns={columns}
+            data={data}
+            pagination
+            paginationServer
+            paginationTotalRows={totalRows}
+            paginationPerPage={perPage}
+            onChangePage={handlePageChange}
+            onChangeRowsPerPage={handleRowsPerPageChange}
+            paginationDefaultPage={currentPage}
+          />
+          {/* <Pagination
             nextPageUrl={nextPageUrl}
             setCurrentPage={setCurrentPage}
             prevPageUrl={prevPageUrl}
             lastPage={lastPage}
             currentPage={currentPage}
-          />
+          /> */}
         </>
       )}
     </section>
@@ -124,83 +153,83 @@ const District = () => {
 
 export default District;
 
-const Pagination = ({
-  nextPageUrl,
-  setCurrentPage,
-  prevPageUrl,
-  lastPage,
-  currentPage,
-}) => {
-  const handleNext = () => {
-    if (nextPageUrl) {
-      setCurrentPage((prevPage) => prevPage + 1);
-    }
-  };
+// const Pagination = ({
+//   nextPageUrl,
+//   setCurrentPage,
+//   prevPageUrl,
+//   lastPage,
+//   currentPage,
+// }) => {
+//   const handleNext = () => {
+//     if (nextPageUrl) {
+//       setCurrentPage((prevPage) => prevPage + 1);
+//     }
+//   };
 
-  const handlePrev = () => {
-    if (prevPageUrl) {
-      setCurrentPage((prevPage) => prevPage - 1);
-    }
-  };
-  // pagination store
-  useEffect(() => {
-    sessionStorage.setItem("currentPage", currentPage);
-  }, [currentPage]);
-  return (
-    <>
-      <div className="flex justify-end items-center gap-2 my-3">
-        <button
-          onClick={() => {
-            setCurrentPage(1);
-          }}
-          disabled={!prevPageUrl}
-          className={`px-2 py-2 rounded-lg font-medium text-white transition-all duration-300 ${
-            !prevPageUrl
-              ? "bg-gray-400 cursor-not-allowed"
-              : "bg-blue-500 hover:bg-blue-600"
-          }`}
-        >
-          <FaAngleDoubleLeft />
-        </button>
-        <button
-          onClick={handlePrev}
-          disabled={!prevPageUrl}
-          className={`px-2 py-2 rounded-lg font-medium text-white transition-all duration-300 ${
-            !prevPageUrl
-              ? "bg-gray-400 cursor-not-allowed"
-              : "bg-blue-500 hover:bg-blue-600"
-          }`}
-        >
-          <IoIosArrowBack />
-        </button>
-        <span className="text-gray-700">
-          Page {currentPage} of {lastPage}
-        </span>
-        <button
-          onClick={handleNext}
-          disabled={!nextPageUrl}
-          className={`px-2 py-2 rounded-lg font-medium text-white transition-all duration-300 ${
-            !nextPageUrl
-              ? "bg-gray-400 cursor-not-allowed"
-              : "bg-blue-500 hover:bg-blue-600"
-          }`}
-        >
-          <IoIosArrowForward />
-        </button>
-        <button
-          onClick={() => {
-            setCurrentPage(lastPage);
-          }}
-          disabled={!nextPageUrl}
-          className={`px-2 py-2 rounded-lg font-medium text-white transition-all duration-300 ${
-            !nextPageUrl
-              ? "bg-gray-400 cursor-not-allowed"
-              : "bg-blue-500 hover:bg-blue-600"
-          }`}
-        >
-          <FaAngleDoubleRight />
-        </button>
-      </div>
-    </>
-  );
-};
+//   const handlePrev = () => {
+//     if (prevPageUrl) {
+//       setCurrentPage((prevPage) => prevPage - 1);
+//     }
+//   };
+//   // pagination store
+//   useEffect(() => {
+//     sessionStorage.setItem("currentPage", currentPage);
+//   }, [currentPage]);
+//   return (
+//     <>
+//       <div className="flex justify-end items-center gap-2 my-3">
+//         <button
+//           onClick={() => {
+//             setCurrentPage(1);
+//           }}
+//           disabled={!prevPageUrl}
+//           className={`px-2 py-2 rounded-lg font-medium text-white transition-all duration-300 ${
+//             !prevPageUrl
+//               ? "bg-gray-400 cursor-not-allowed"
+//               : "bg-blue-500 hover:bg-blue-600"
+//           }`}
+//         >
+//           <FaAngleDoubleLeft />
+//         </button>
+//         <button
+//           onClick={handlePrev}
+//           disabled={!prevPageUrl}
+//           className={`px-2 py-2 rounded-lg font-medium text-white transition-all duration-300 ${
+//             !prevPageUrl
+//               ? "bg-gray-400 cursor-not-allowed"
+//               : "bg-blue-500 hover:bg-blue-600"
+//           }`}
+//         >
+//           <IoIosArrowBack />
+//         </button>
+//         <span className="text-gray-700">
+//           Page {currentPage} of {lastPage}
+//         </span>
+//         <button
+//           onClick={handleNext}
+//           disabled={!nextPageUrl}
+//           className={`px-2 py-2 rounded-lg font-medium text-white transition-all duration-300 ${
+//             !nextPageUrl
+//               ? "bg-gray-400 cursor-not-allowed"
+//               : "bg-blue-500 hover:bg-blue-600"
+//           }`}
+//         >
+//           <IoIosArrowForward />
+//         </button>
+//         <button
+//           onClick={() => {
+//             setCurrentPage(lastPage);
+//           }}
+//           disabled={!nextPageUrl}
+//           className={`px-2 py-2 rounded-lg font-medium text-white transition-all duration-300 ${
+//             !nextPageUrl
+//               ? "bg-gray-400 cursor-not-allowed"
+//               : "bg-blue-500 hover:bg-blue-600"
+//           }`}
+//         >
+//           <FaAngleDoubleRight />
+//         </button>
+//       </div>
+//     </>
+//   );
+// };
--- src/pages/address/division/index.jsx
+++ src/pages/address/division/index.jsx
@@ -22,9 +22,7 @@ const Division = () => {
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const [lastPage, setLastPage] = useState(1);
-  const [nextPageUrl, setNextPageUrl] = useState(null);
-  const [prevPageUrl, setPrevPageUrl] = useState(null);
+
 
   // fetch banner data
   const fetchData = useCallback(
@@ -35,9 +33,6 @@ const Division = () => {
         if (response?.status === 200 || response?.status === 201) {
           setData(response?.data?.data?.data);
           setCurrentPage(response?.data?.data?.current_page);
-          setLastPage(response?.data?.data?.last_page);
-          setNextPageUrl(response?.data?.data?.next_page_url);
-          setPrevPageUrl(response?.data?.data?.prev_page_url);
           setLoading(false);
         }
       } catch (error) {
@@ -108,13 +103,13 @@ const Division = () => {
       ) : (
         <>
           <DataTable columns={columns} data={data} />
-          <Pagination
+          {/* <Pagination
             nextPageUrl={nextPageUrl}
             setCurrentPage={setCurrentPage}
             prevPageUrl={prevPageUrl}
             lastPage={lastPage}
             currentPage={currentPage}
-          />
+          /> */}
         </>
       )}
     </section>
@@ -123,83 +118,83 @@ const Division = () => {
 
 export default Division;
 
-const Pagination = ({
-  nextPageUrl,
-  setCurrentPage,
-  prevPageUrl,
-  lastPage,
-  currentPage,
-}) => {
-  const handleNext = () => {
-    if (nextPageUrl) {
-      setCurrentPage((prevPage) => prevPage + 1);
-    }
-  };
+// const Pagination = ({
+//   nextPageUrl,
+//   setCurrentPage,
+//   prevPageUrl,
+//   lastPage,
+//   currentPage,
+// }) => {
+//   const handleNext = () => {
+//     if (nextPageUrl) {
+//       setCurrentPage((prevPage) => prevPage + 1);
+//     }
+//   };
 
-  const handlePrev = () => {
-    if (prevPageUrl) {
-      setCurrentPage((prevPage) => prevPage - 1);
-    }
-  };
-  // pagination store
-  useEffect(() => {
-    sessionStorage.setItem("currentPage", currentPage);
-  }, [currentPage]);
-  return (
-    <>
-      <div className="flex justify-end items-center gap-2 my-3">
-        <button
-          onClick={() => {
-            setCurrentPage(1);
-          }}
-          disabled={!prevPageUrl}
-          className={`px-2 py-2 rounded-lg font-medium text-white transition-all duration-300 ${
-            !prevPageUrl
-              ? "bg-gray-400 cursor-not-allowed"
-              : "bg-blue-500 hover:bg-blue-600"
-          }`}
-        >
-          <FaAngleDoubleLeft />
-        </button>
-        <button
-          onClick={handlePrev}
-          disabled={!prevPageUrl}
-          className={`px-2 py-2 rounded-lg font-medium text-white transition-all duration-300 ${
-            !prevPageUrl
-              ? "bg-gray-400 cursor-not-allowed"
-              : "bg-blue-500 hover:bg-blue-600"
-          }`}
-        >
-          <IoIosArrowBack />
-        </button>
-        <span className="text-gray-700">
-          Page {currentPage} of {lastPage}
-        </span>
-        <button
-          onClick={handleNext}
-          disabled={!nextPageUrl}
-          className={`px-2 py-2 rounded-lg font-medium text-white transition-all duration-300 ${
-            !nextPageUrl
-              ? "bg-gray-400 cursor-not-allowed"
-              : "bg-blue-500 hover:bg-blue-600"
-          }`}
-        >
-          <IoIosArrowForward />
-        </button>
-        <button
-          onClick={() => {
-            setCurrentPage(lastPage);
-          }}
-          disabled={!nextPageUrl}
-          className={`px-2 py-2 rounded-lg font-medium text-white transition-all duration-300 ${
-            !nextPageUrl
-              ? "bg-gray-400 cursor-not-allowed"
-              : "bg-blue-500 hover:bg-blue-600"
-          }`}
-        >
-          <FaAngleDoubleRight />
-        </button>
-      </div>
-    </>
-  );
-};
+//   const handlePrev = () => {
+//     if (prevPageUrl) {
+//       setCurrentPage((prevPage) => prevPage - 1);
+//     }
+//   };
+//   // pagination store
+//   useEffect(() => {
+//     sessionStorage.setItem("currentPage", currentPage);
+//   }, [currentPage]);
+//   return (
+//     <>
+//       <div className="flex justify-end items-center gap-2 my-3">
+//         <button
+//           onClick={() => {
+//             setCurrentPage(1);
+//           }}
+//           disabled={!prevPageUrl}
+//           className={`px-2 py-2 rounded-lg font-medium text-white transition-all duration-300 ${
+//             !prevPageUrl
+//               ? "bg-gray-400 cursor-not-allowed"
+//               : "bg-blue-500 hover:bg-blue-600"
+//           }`}
+//         >
+//           <FaAngleDoubleLeft />
+//         </button>
+//         <button
+//           onClick={handlePrev}
+//           disabled={!prevPageUrl}
+//           className={`px-2 py-2 rounded-lg font-medium text-white transition-all duration-300 ${
+//             !prevPageUrl
+//               ? "bg-gray-400 cursor-not-allowed"
+//               : "bg-blue-500 hover:bg-blue-600"
+//           }`}
+//         >
+//           <IoIosArrowBack />
+//         </button>
+//         <span className="text-gray-700">
+//           Page {currentPage} of {lastPage}
+//         </span>
+//         <button
+//           onClick={handleNext}
+//           disabled={!nextPageUrl}
+//           className={`px-2 py-2 rounded-lg font-medium text-white transition-all duration-300 ${
+//             !nextPageUrl
+//               ? "bg-gray-400 cursor-not-allowed"
+//               : "bg-blue-500 hover:bg-blue-600"
+//           }`}
+//         >
+//           <IoIosArrowForward />
+//         </button>
+//         <button
+//           onClick={() => {
+//             setCurrentPage(lastPage);
+//           }}
+//           disabled={!nextPageUrl}
+//           className={`px-2 py-2 rounded-lg font-medium text-white transition-all duration-300 ${
+//             !nextPageUrl
+//               ? "bg-gray-400 cursor-not-allowed"
+//               : "bg-blue-500 hover:bg-blue-600"
+//           }`}
+//         >
+//           <FaAngleDoubleRight />
+//         </button>
+//       </div>
+//     </>
+//   );
+// };
